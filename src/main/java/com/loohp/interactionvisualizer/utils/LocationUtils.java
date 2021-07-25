aa85ff44fefe4ae733102ecfa1566a98f081481a
LOOHP <jamesloohp@gmail.com>
2021-07-25 12:23:39 +0800

    Fixed location.isWorldLoaded() not present in legacy versions of Minecraft

--- src/main/java/com/loohp/interactionvisualizer/utils/LocationUtils.java
+++ src/main/java/com/loohp/interactionvisualizer/utils/LocationUtils.java
@@ -5,7 +5,11 @@ import org.bukkit.Location;
 public class LocationUtils {
 	
 	public static boolean isLoaded(Location location) {
-		return location.isWorldLoaded() && location.getWorld().isChunkLoaded(location.getBlockX() >> 4, location.getBlockZ() >> 4);
+		try {
+			return location.getWorld() != null && location.getWorld().isChunkLoaded(location.getBlockX() >> 4, location.getBlockZ() >> 4);
+		} catch (IllegalArgumentException e) {
+			return false;
+		}
 	}
 
 }
